f3079d1217968589c5680a18a77fed53c37131bc
Daniel Vega <danielalexvega@gmail.com>
2021-08-01 16:32:28 -0500

    add validation to incoming input with express validator

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,5 +1,6 @@
 const HttpError = require("../models/http-error");
 const { v4: uuidv4 } = require("uuid");
+const { validationResult } = require("express-validator");
 
 let DUMMY_PLACES = [
   {
@@ -44,6 +45,11 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError("Invalid inputs passed, please check your data", 422);
+  }
+
   const { title, description, coordinates, address, creator } = req.body;
   const createdPlace = {
     id: uuidv4(),
@@ -60,6 +66,11 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError("Invalid inputs passed, please check your data", 422);
+  }
+
   const placeId = req.params.placeId;
   const { title, description } = req.body;
 
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const { check } = require("express-validator");
 
-const placesControllers = require('../controllers/places-controller');
+const placesControllers = require("../controllers/places-controller");
 
 const router = express.Router();
 
@@ -8,10 +9,22 @@ router.get("/:placeId", placesControllers.getPlaceById);
 
 router.get("/user/:userId", placesControllers.getPlacesByUserId);
 
-router.post("/", placesControllers.createPlace);
+router.post(
+  "/",
+  [
+    check("title").not().isEmpty(),
+    check("description").isLength({ min: 5 }),
+    check("address").not().isEmpty(),
+  ],
+  placesControllers.createPlace
+);
 
-router.patch("/:placeId", placesControllers.updatePlace);
+router.patch(
+  "/:placeId",
+  [check("title").not().isEmpty(), check("description").isLength({ min: 5 })],
+  placesControllers.updatePlace
+);
 
-router.delete("/:placeId", placesControllers.deletePlace)
+router.delete("/:placeId", placesControllers.deletePlace);
 
 module.exports = router;
