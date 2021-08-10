27f76736abf97dc10050fbbf4f223d32f74636fa
Daniel Vega <danielalexvega@gmail.com>
2021-08-10 08:59:01 -0500

    add middleware to handle cors

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
@@ -30,11 +40,13 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occurred!" });
 });
 
-
-mongoose.connect('mongodb+srv://mern-daniel:2T4jR92YdZwgsrYF@vega-cluster.t7ry1.mongodb.net/places?retryWrites=true&w=majority')
-.then(()=> {
-  app.listen(5000);
-})
-.catch(err => {
-  console.log(err);
-});
+mongoose
+  .connect(
+    "mongodb+srv://mern-daniel:2T4jR92YdZwgsrYF@vega-cluster.t7ry1.mongodb.net/mern?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
